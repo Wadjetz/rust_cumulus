1b49fe0c2eab2b628ff40bd3f354797d11e665cd
Egor BEREZOVSKIY <ebe@zengularity.com>
2017-06-14 18:28:25 +0200

    Add juniper test exemple

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,34 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+use rocket::response::content;
+use rocket::State;
+
+extern crate juniper;
+use juniper::{EmptyMutation, RootNode};
+use juniper::rocket_handlers;
+use juniper::tests::model::Database;
+
+type Schema = RootNode<'static, Database, EmptyMutation<Database>>;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn graphiql() -> content::HTML<String> {
+    rocket_handlers::graphiql_source("/graphql")
+}
+
+#[post("/graphql", data="<request>")]
+fn post_graphql_handler(
+    context: State<Database>,
+    request: rocket_handlers::GraphQLRequest,
+    schema: State<Schema>
+) -> rocket_handlers::GraphQLResponse {
+    request.execute(&schema, &context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .manage(Database::new())
+        .manage(Schema::new(Database::new(), EmptyMutation::<Database>::new()))
+        .mount("/", routes![graphiql, post_graphql_handler])
+        .launch();
 }
