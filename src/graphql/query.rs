4e7395da0d816789a548d8162a9d819ca11d25bd
Egor Berezovskiy <egor.neon@gmail.com>
2017-08-06 12:08:58 +0200

    refactor auth_query_resolver

--- src/graphql/query.rs
+++ src/graphql/query.rs
@@ -3,12 +3,10 @@ use r2d2_postgres::PostgresConnectionManager;
 use r2d2::Pool;
 
 use graphql::auth_query_ql::AuthQuery;
-use repositories::user_repository::{verify_user};
 use sources::Source;
 use std::error::Error;
 use sources::find_sources_resolver;
-use users::login_resolver;
-use token;
+use users::{auth_query_resolver, login_resolver};
 
 pub struct Query {
     pub connection: Pool<PostgresConnectionManager>,
@@ -31,11 +29,7 @@ graphql_object!(Query: Query as "Query" |&self| {
         &executor,
         token: String as "Auth token"
     ) -> Result<AuthQuery, String> as "Auth" {
-      let connection = executor.context().connection.clone().get().expect("Error connection pool");
-      token::decode_auth(&token)
-            .and_then(|auth_data| verify_user(&connection, auth_data))
-            .map(AuthQuery::new)
-            .map_err(|e| e.description().to_string())
+        auth_query_resolver(executor, token).map_err(|e| e.description().to_string())
     }
 
     field login(
