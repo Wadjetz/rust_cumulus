c7027b0fc8c5a532c0ad45d1e1536f0990e23526
Egor Berezovskiy <egor.neon@gmail.com>
2018-02-16 13:24:51 +0100

    fix like feed

--- client/src/mindstream/MindStreamReducer.ts
+++ client/src/mindstream/MindStreamReducer.ts
@@ -21,26 +21,28 @@ const MindStreamReducer = (state: MindStreamState = initState, action: MindStrea
         case "LOAD_UNREADED_FEEDS_SUCCESS": return { ...state, feeds: action.feeds, loading: false }
         case "LOAD_UNREADED_FEEDS_ERROR": return { ...state, loading: false, error: action.error }
         case "READ_FEED": return { ...state, loading: true }
+        case "READ_FEED_SUCCESS": return nextFeedReduce(state, action)
         case "READ_FEED_ERROR": return { ...state, loading: false, error: action.error }
         case "LOAD_UNREADED_FEEDS_BY_SOURCE": return { ...state, loading: true }
         case "LOAD_UNREADED_FEEDS_BY_SOURCE_SUCCESS": return { ...state, feeds: action.feeds, loading: false }
         case "LOAD_UNREADED_FEEDS_BY_SOURCE_ERROR": return { ...state, loading: false, error: action.error }
-
-        case "NEXT_FEED": {
-            const [first, ...rest] = state.feeds
-            return { ...state, feeds: rest, previusFeeds: [first, ...state.previusFeeds] }
-        }
-
-        case "PREVIOUS_FEED": {
-            const [first, ...rest] = state.previusFeeds
-            if (first) {
-                return { ...state, feeds: [first, ...state.feeds], previusFeeds: rest }
-            } else {
-                return state
-            }
-        }
-
+        case "NEXT_FEED": return nextFeedReduce(state, action)
+        case "PREVIOUS_FEED": return previousFeedReduce(state, action)
         default: return state
+    }
+}
+
+function nextFeedReduce(state: MindStreamState, action: MindStreamAction): MindStreamState {
+    const [first, ...rest] = state.feeds
+    return { ...state, feeds: rest, previusFeeds: [first, ...state.previusFeeds] }
+}
+
+function previousFeedReduce(state: MindStreamState, action: MindStreamAction): MindStreamState {
+    const [first, ...rest] = state.previusFeeds
+    if (first) {
+        return { ...state, feeds: [first, ...state.feeds], previusFeeds: rest }
+    } else {
+        return state
     }
 }
 
